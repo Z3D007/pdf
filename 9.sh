42e09ef1c75a115f88c2eed26e0c023621052bf5
minkhaingkhantpro <87145482+minkhaingkhantpro@users.noreply.github.com>
2021-07-25 18:27:37 +0000

    commit text

---
+++ 9.sh
@@ -0,0 +1,257 @@
+clear
+#  coder by M4CK3R
+# Colours
+#-----------------
+red="\e[91m"
+rset="\e[0m"
+grn="\e[92m"
+ylo="\e[93m"
+blue="\e[94m"
+cyan="\e[96m"
+pink="\e[95m"
+#-----------------
+
+echo -e '\e[95m
+
+██████╗░██████╗░███████╗████╗██████╗░░█████╗░████╗
+██╔══██╗██╔══██╗██╔════╝██╔═╝╚════██╗██╔══██╗╚═██║
+██████╔╝██║░░██║█████╗░░██║░░░░███╔═╝██║░░██║░░██║
+██╔═══╝░██║░░██║██╔══╝░░██║░░██╔══╝░░██║░░██║░░██║
+██║░░░░░██████╔╝██║░░░░░████╗███████╗╚█████╔╝████║
+╚═╝░░░░░╚═════╝░╚═╝░░░░░╚═══╝╚══════╝░╚════╝░╚═══╝'
+echo -e '\e[93m--------------------------------------------------'
+echo -e '\e[96m                 #Coder By M4CK3R#'
+echo -e '\e[93m--------------------------------------------------'
+echo ""
+echo -e '\e[94m[1]==>Python Basic'
+echo -e '\e[94m[2]==>Python Basic(2)'
+echo -e '\e[94m[3]==>Metasploit Penetration Testing'
+echo -e '\e[94m[4]==>Create BotNet '
+echo -e '\e[94m[5]==>Wifi Hacking'
+echo -e '\e[94m[6]==>Wifi Hacking(2)'
+echo -e '\e[94m[7]==>Create Your Own Virus'
+echo -e '\e[94m[8]==>Basic Hacking'
+echo -e '\e[94m[9]==>The harverster Information Gathering'
+echo -e '\e[94m[10]==>Hacking Myanmar'
+echo -e '\e[94m[11]==>Basic Html'
+echo -e '\e[94m[12]==>Telnet'
+echo -e '\e[94m[13]==>Wep Wifi Hacking'
+echo -e '\e[94m[14]==>Poison The Network'
+echo -e '\e[94m[15]==>Networking Hacking Testing'
+echo -e '\e[94m[16]==>NotePad Hacking 101'
+echo -e '\e[94m[17]==>Networking'
+echo -e '\e[94m[18]==>Kali Linux'
+echo -e '\e[94m[19]==>ISS website Hacking'
+echo -e '\e[94m[20]==>DVWA'
+echo -e '\e[94m[21]==>Exit'
+              
+read -p "[M4CK3R]=>" sad
+
+if [ $sad = 1 ] || [ $sad = 1 ]
+then
+clear
+echo "Python Basic PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1OBOja2R97jDVHMpKMSI5ptDd2U25bCSz/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 2 ] || [ $sad = 2 ]
+then
+clear
+echo "Python Basic(2) PDF Loading..."
+sleep 1
+termux-open https://bit.ly/3gxW5cB
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 3 ] || [ $sad = 3 ]
+then
+clear
+echo "Metasploit Penetration Testing PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1MWBaaOG_tpFMOb50W54dF3XQn8PtDu5u/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 4 ] || [ $sad = 4 ]
+then
+clear
+echo "Create BotNet PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1LICWtOqBOdjGL6SCv4V2s4rRppogVy6q/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 5 ] || [ $sad = 5 ]
+then
+clear
+echo "Wifi Hacking PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1N89fTmtMdlSHW83afWZO9cFxzvFvrbJ2/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 6 ] || [ $sad = 6 ]
+then
+clear
+echo "Wifi Hacking(2) PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1NC5kjiAlCelxwpAJQuEJjtHIvT-NEi7p/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 7 ] || [ $sad = 7 ]
+then
+clear
+echo "Create Your Own Virus PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1LLy_SDAHhoB0UtfTtwcDk25oO72UKep6/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 8 ] || [ $sad = 8 ]
+then
+clear
+echo "Basic Hacking PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1LBqFcrkP2Y3dp-cKU58J2b2QvNu2T51M/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 9 ] || [ $sad = 9 ]
+then
+clear
+echo "The harverster Information Gathering PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1LLL8jc1Y8kbcndtKR7Hvwh7XbAXQI-23/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 10 ] || [ $sad = 10 ]
+then
+clear
+echo "Hacking Myanmar PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1Le0Om84eYdmB-dlVE4PI9-hs3gVikNu9/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+
+if [ $sad = 11 ] || [ $sad = 11 ]
+then
+clear
+echo "Basic Html PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1KmpnQ22Eww9laXQ629IRQP63-TpksS7h/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 12 ] || [ $sad = 12 ]
+then
+clear
+echo "Telnet PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1NSCL9KfmhT2teynSK-Vu2EALgUDzNBsg/view?usp=drivesdk
+Sleep4
+bash 9.sh
+fi
+
+if [ $sad = 13 ] || [ $sad = 13 ]
+then
+clear
+echo "Wep Wifi Hacking PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1N7jjE_cI-pBhdhLxNPV1JqcKPqsJ_tBu/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 14 ] || [ $sad = 14 ]
+then
+clear
+echo "Poison The Network PDF Loading"
+sleep 1
+termux-open https://drive.google.com/file/d/1Mn-9vndB8ZGGw3Ht5UebMQOlRX9UwsGa/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 15 ] || [ $sad = 15 ]
+then
+clear
+echo "Networking Hacking Testing PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1MYTntsyA1olEvm7yxn19ETP9o5OtbC3V/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 16 ] || [ $sad = 16 ]
+then
+clear
+echo "NotePad Hacking 101 PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1MhuRhFpeG6tE9SjO4--2U8yDLFzFz2I-/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 17 ] || [ $sad = 17 ]
+then
+clear
+echo "Networking PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1MfV7uTES58__sEcw8havkL44Pao9wTMd/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 18 ] || [ $sad = 18 ]
+then
+clear
+echo "Kali Linux PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1MOUa-6vbuc3mi4oqoj5p8TyVgXkU5W27/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 19 ] || [ $sad = 19 ]
+then
+clear
+echo "ISS website Hacking PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1M52RmNDtzMrA-ixAK6_o0DVJRDhTyZC1/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 20 ] || [ $sad = 20 ]
+then
+clear
+echo "DVWA PDF Loading..."
+sleep 1
+termux-open https://drive.google.com/file/d/1LSf5ivKkTj5W2zVzIf2AdWbRNc3nCtYT/view?usp=drivesdk
+sleep 4
+bash 9.sh
+fi
+
+if [ $sad = 21 ] || [ $sad = 21 ]
+then
+echo "GOOD BY TOOL USER #M4CK3R"
+sleep 2
+clear
+exit
+fi
